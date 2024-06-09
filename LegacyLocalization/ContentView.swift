a02eef12ba4d675b6b8de10ddbee941f57b7cf19
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-06-09 12:56:44 +0600

    project init

---
+++ LegacyLocalization/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  LegacyLocalization
+//
+//  Created by Shah Md Imran Hossain on 9/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var name: LocalizedStringResource = "Dear"
+    @State private var loveCounter = 3000
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                Text("Do you know \(name)?")
+                    .font(.title)
+                
+                Spacer()
+                Spacer()
+                Spacer()
+                
+                HStack {
+                    Text("I love you")
+                    Text("\(loveCounter)")
+                        .bold()
+                        .foregroundStyle(Color.red)
+                }
+                
+                Spacer()
+                Spacer()
+                Spacer()
+                
+                HStack {
+                    Button("Reset") {
+                        loveCounter = 3000
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Random") {
+                        loveCounter = Int.random(in: 1...999999999)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle("Localization App")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LegacyLocalization/LegacyLocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegacyLocalizationApp.swift
+//  LegacyLocalization
+//
+//  Created by Shah Md Imran Hossain on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LegacyLocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
